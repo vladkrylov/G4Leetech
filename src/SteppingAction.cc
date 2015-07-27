86578106a9015dbf0220f6d1220da0fcf614f5db
vlad009 <vladkrilov9@gmail.com>
2015-07-27 17:29:16 +0300

    SteppingAction rewritten

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -1,81 +1,72 @@
-#include "SteppingAction.hh"
-
-#include "DetectorConstruction.hh"
-#include "EventAction.hh"
-#include "RunAction.hh"
-
 #include "G4Step.hh"
 #include "G4Track.hh"
 #include "G4RunManager.hh"
 #include "G4ParticleDefinition.hh"
 
-//root
 #include "TTree.h"
 
+#include "SteppingAction.hh"
+#include "DetectorConstruction.hh"
+#include "EventAction.hh"
+#include "RunAction.hh"
 
 SteppingAction::SteppingAction(){
   detector = (DetectorConstruction*)
     G4RunManager::GetRunManager()->GetUserDetectorConstruction();
-  eventaction = (EventAction*)
+  eventAction = (EventAction*)
     G4RunManager::GetRunManager()->GetUserEventAction();	       
-  runaction = (RunAction*)G4RunManager::GetRunManager()->GetUserRunAction();
+  runAction = (RunAction*)G4RunManager::GetRunManager()->GetUserRunAction();
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 SteppingAction::~SteppingAction()
 { }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-void SteppingAction::UserSteppingAction(const G4Step* aStep){
+void SteppingAction::UserSteppingAction(const G4Step* aStep)
+{
+	// get volume of the current step
+	G4VPhysicalVolume* volume
+		= aStep->GetPreStepPoint()->GetTouchableHandle()->GetVolume();
+	//G4cout<<volume->GetName()<<G4endl;
   
-  // get volume of the current step
-  G4VPhysicalVolume* volume 
-    = aStep->GetPreStepPoint()->GetTouchableHandle()->GetVolume();
-  //G4cout<<volume->GetName()<<G4endl;
-  
-  runaction->HitInfo.IsInDetector = 0;
-  runaction->HitInfo.TypeID   = aStep->GetTrack()->GetDefinition()->GetPDGEncoding();
-//fill the para,eters of the step
-  if(volume->GetName()=="SenDet1"){ //|| volume->GetName()=="SenDet2"){
-    //G4cout<<"### Run "<<G4RunManager::GetRunManager()->GetRunID()<<G4endl
-    //	  <<aStep->GetTrack()->GetKineticEnergy()<<G4endl;    
-	//	G4cout<<aStep->GetTrack()->GetDefinition()->GetPDGEncoding()<<std::endl;
-	  if(aStep->GetTrack()->GetDefinition()->GetPDGEncoding()==11)
-	  {
-		  	runaction->HitInfo.IsInDetector = 1;
-		  	runaction->HitInfo.EventID  = eventaction->_evtNb;
-			runaction->HitInfo.TypeID   = aStep->GetTrack()->GetDefinition()->GetPDGEncoding();
-                        if(aStep->GetTrack()->GetDefinition()->GetPDGEncoding()==22)G4cout<<"gamma was 100%\n";
-			runaction->HitInfo.TrackID  = aStep->GetTrack()->GetTrackID();
-			runaction->HitInfo.ParentID = aStep->GetTrack()->GetParentID();
-			runaction->HitInfo.Energy   = aStep->GetTrack()->GetKineticEnergy();
-			runaction->HitInfo.Time     = aStep->GetPostStepPoint()->GetGlobalTime();
-			runaction->HitInfo.PosX     = aStep->GetPostStepPoint()->GetPosition().getX();
-			runaction->HitInfo.PosY     = aStep->GetPostStepPoint()->GetPosition().getY();
-			runaction->HitInfo.PosZ     = aStep->GetPostStepPoint()->GetPosition().getZ();
-			runaction->HitInfo.PX       = aStep->GetPostStepPoint()->GetMomentum().getX();
-			runaction->HitInfo.PY       = aStep->GetPostStepPoint()->GetMomentum().getY();
-			runaction->HitInfo.PZ       = aStep->GetPostStepPoint()->GetMomentum().getZ();
-	
-		double Pe=sqrt(runaction->HitInfo.PZ*runaction->HitInfo.PZ+
-						 runaction->HitInfo.PY*runaction->HitInfo.PY+
-						 runaction->HitInfo.PX*runaction->HitInfo.PX);
-		 runaction->HitInfo.P = Pe;
-	
-	double Tet = acos(runaction->HitInfo.PZ/Pe)*180/3.14;
-		runaction->HitInfo.Theta = Tet;
+	runAction->HitInfo.IsInDetector = 0;
+	runAction->HitInfo.TypeID   = aStep->GetTrack()->GetDefinition()->GetPDGEncoding();
 
-		double PosXYnow = sqrt(runaction->HitInfo.PosX*runaction->HitInfo.PosX+runaction->HitInfo.PosY*runaction->HitInfo.PosY);
-		runaction->HitInfo.PosXY = PosXYnow;
+	//fill the parameters of the step
+	if(volume->GetName()=="SenDet1") { //|| volume->GetName()=="SenDet2"){
+		//G4cout<<"### Run "<<G4RunManager::GetRunManager()->GetRunID()<<G4endl
+		//	  <<aStep->GetTrack()->GetKineticEnergy()<<G4endl;
+		//	G4cout<<aStep->GetTrack()->GetDefinition()->GetPDGEncoding()<<std::endl;
+		if(aStep->GetTrack()->GetDefinition()->GetPDGEncoding()==11) {
+			runAction->HitInfo.IsInDetector = 1;
+		  	runAction->HitInfo.EventID  = eventAction->_evtNb;
+			runAction->HitInfo.TypeID   = aStep->GetTrack()->GetDefinition()->GetPDGEncoding();
+			if(aStep->GetTrack()->GetDefinition()->GetPDGEncoding() == 22)
+				G4cout<<"gamma was 100%\n";
+			runAction->HitInfo.TrackID  = aStep->GetTrack()->GetTrackID();
+			runAction->HitInfo.ParentID = aStep->GetTrack()->GetParentID();
+			runAction->HitInfo.Energy   = aStep->GetTrack()->GetKineticEnergy();
+			runAction->HitInfo.Time     = aStep->GetPostStepPoint()->GetGlobalTime();
+			runAction->HitInfo.PosX     = aStep->GetPostStepPoint()->GetPosition().getX();
+			runAction->HitInfo.PosY     = aStep->GetPostStepPoint()->GetPosition().getY();
+			runAction->HitInfo.PosZ     = aStep->GetPostStepPoint()->GetPosition().getZ();
+			runAction->HitInfo.PX       = aStep->GetPostStepPoint()->GetMomentum().getX();
+			runAction->HitInfo.PY       = aStep->GetPostStepPoint()->GetMomentum().getY();
+			runAction->HitInfo.PZ       = aStep->GetPostStepPoint()->GetMomentum().getZ();
+	
+			double Pe=sqrt(runAction->HitInfo.PZ*runAction->HitInfo.PZ+
+						   runAction->HitInfo.PY*runAction->HitInfo.PY+
+						   runAction->HitInfo.PX*runAction->HitInfo.PX);
+			runAction->HitInfo.P = Pe;
+	
+			double Tet = acos(runAction->HitInfo.PZ/Pe)*180/3.14;
+			runAction->HitInfo.Theta = Tet;
 
-			runaction->tree->Fill();
+			double PosXYnow = sqrt(runAction->HitInfo.PosX*runAction->HitInfo.PosX+runAction->HitInfo.PosY*runAction->HitInfo.PosY);
+			runAction->HitInfo.PosXY = PosXYnow;
+
+			runAction->tree->Fill();
 		}
-			aStep->GetTrack()->SetTrackStatus(fStopAndKill);
-	  }
-  
-
+		aStep->GetTrack()->SetTrackStatus(fStopAndKill);
+	}
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
