fa3603df27195fd5cfe5f34f11c063c3645d6744
vlad009 <vladkrilov9@gmail.com>
2015-07-27 16:50:32 +0300

    MagneticField rewritten

--- include/MagneticField.hh
+++ include/MagneticField.hh
@@ -7,42 +7,40 @@
 class MagneticField : public G4MagneticField
 {
 public:
-  MagneticField();
-  ~MagneticField();
-  
-  virtual void GetFieldValue( const  double Point[4],	//good name
-			      double *Bfield ) const;
-  
-									//set field - why bool???
-  bool SetField( const double *Point, G4double & Bx, G4double & By, G4double & Bz) const;
+	MagneticField();
+	~MagneticField();
 
-  void setBfieldY(G4double byVal); // set B at Y axis
+	virtual void GetFieldValue( const  double Point[4],	//good name
+				  double *Bfield ) const;
 
-  void setFieldBox1( G4double valxc, G4double valyc, G4double valzc,		//setting size of volume woth field
-		     G4double valdx, G4double valdy, G4double valdz);
-  void setFieldBox2( G4double valxc, G4double valyc, G4double valzc,
-		     G4double valdx, G4double valdy, G4double valdz);
+	//TODO set field - why bool???
+	bool SetField( const double *Point, G4double & Bx, G4double & By, G4double & Bz) const;
+
+	void setBfieldY(G4double byVal); // set B at Y axis
+
+	void setFieldBox1( G4double valxc, G4double valyc, G4double valzc,		//setting size of volume woth field
+			 G4double valdx, G4double valdy, G4double valdz);
+	void setFieldBox2( G4double valxc, G4double valyc, G4double valzc,
+			 G4double valdx, G4double valdy, G4double valdz);
 
 private:
-  
-  G4double _BfieldY;
+	G4double _BfieldY;
 
-  G4double _xc1;
-  G4double _yc1;
-  G4double _zc1;
+	G4double _xc1;
+	G4double _yc1;
+	G4double _zc1;
 
-  G4double _dx1;
-  G4double _dy1;
-  G4double _dz1;
+	G4double _dx1;
+	G4double _dy1;
+	G4double _dz1;
 
-  G4double _xc2;
-  G4double _yc2;
-  G4double _zc2;
+	G4double _xc2;
+	G4double _yc2;
+	G4double _zc2;
 
-  G4double _dx2;
-  G4double _dy2;
-  G4double _dz2;
-
+	G4double _dx2;
+	G4double _dy2;
+	G4double _dz2;
 };
 
 
