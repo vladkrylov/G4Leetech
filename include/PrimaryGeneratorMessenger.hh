733d9ce7861b21b7cfa78d591e0777bddc068b95
vlad009 <vladkrilov9@gmail.com>
2015-07-27 17:16:16 +0300

    PrimaryGeneratorMessenger rewritten

--- include/PrimaryGeneratorMessenger.hh
+++ include/PrimaryGeneratorMessenger.hh
@@ -12,26 +12,26 @@ class G4UIcmdWith3Vector;
 class PrimaryGeneratorMessenger: public G4UImessenger
 {
 public:
-  PrimaryGeneratorMessenger(PrimaryGeneratorAction*);
-  virtual ~PrimaryGeneratorMessenger();
-    
-  void SetNewValue(G4UIcommand*, G4String);
+	PrimaryGeneratorMessenger(PrimaryGeneratorAction*);
+	virtual ~PrimaryGeneratorMessenger();
+
+	void SetNewValue(G4UIcommand*, G4String);
     
 private:
-  PrimaryGeneratorAction* Action;
-  G4UIdirectory*          gunDir; 
-  G4UIcmdWithAString*     PerticleCmd;
-  G4UIcmdWith3Vector*     ParticlePositionCmd;
-  G4UIcmdWith3Vector*     ParticleDirectionCmd;
-  G4UIcmdWith3Vector*     ParticleDirectionRMSCmd;
-  G4UIcmdWith3Vector*     ParticleMomentumCmd;
+	PrimaryGeneratorAction* Action;
+	G4UIdirectory*          gunDir;
+	G4UIcmdWithAString*     PerticleCmd;
+	G4UIcmdWith3Vector*     ParticlePositionCmd;
+	G4UIcmdWith3Vector*     ParticleDirectionCmd;
+	G4UIcmdWith3Vector*     ParticleDirectionRMSCmd;
+	G4UIcmdWith3Vector*     ParticleMomentumCmd;
 
-  //SetParticleName(G4String valS);
-  //SetParticlePosition(G4double xval, G4double yval, G4double zval);
-  //SetParticleDir(G4double xval, G4double yval, G4double zval);
-  //SetParticleDirRMS(G4double xvalRMS, G4double yvalRMS, G4double zvalRMS);
-  //SetParticleMom(G4double valMin, G4double valMax);
-  //G4ThreeVector GetNew3VectorValue
+	//SetParticleName(G4String valS);
+	//SetParticlePosition(G4double xval, G4double yval, G4double zval);
+	//SetParticleDir(G4double xval, G4double yval, G4double zval);
+	//SetParticleDirRMS(G4double xvalRMS, G4double yvalRMS, G4double zvalRMS);
+	//SetParticleMom(G4double valMin, G4double valMax);
+	//G4ThreeVector GetNew3VectorValue
 };
 
 #endif
